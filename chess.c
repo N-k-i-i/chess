d545fad3c188a3348d04f321872d231031a5e4b0
Nikita Plotnikov <Nikita Plotnikov>
2020-02-29 20:54:28 +0700

    Fix Chessboard

--- chess.c
+++ chess.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #define SIZE 10
 
-int main () {
-	char board [SIZE] [SIZE] = {"8rnbqkbnr",
-				    "7pppppppp",
-				    "6        ",
-				    "5        ",
-				    "4        ",
-				    "3        ",
-				    "2PPPPPPPP",
-				    "1RNBQKBMR",
-				    " abcdefgh"}
+int main ()
+ {
+
+	char board[SIZE][SIZE] = {"8|rnbqkbnr",
+				  "7|pppppppp",
+				  "6|        ",
+				  "5|        ",
+				  "4|        ",
+				  "3|        ",
+				  "2|PPPPPPPP",
+				  "1|RNBQKBNR",
+				  "  ________",
+				  "  abcdefgh"};
 	for(int i = 0; i < SIZE ; i++) {
-		for(int j = 0; j < SIZE ; i++) {
-			printf("%c ", board [i] [j];
+		for(int j = 0; j < SIZE ; j++) {
+			printf("%c ", board[i][j]);
 		}
 		printf("\n");
 	}
 
-return 0;
+	return 0;
 }
 
